e7e48a5f95a17086f6edaa69723a495be2981041
German Guzelj <german.guzelj@almundo.com>
2015-10-18 03:59:44 -0300

    some new projects..

---
+++ src/process/controller/src/Controller.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Fifos/Fifo.h>
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
---
+++ src/process/controllerQueue/src/ControllerQueue.cpp
@@ -0,0 +1,8 @@
+#include <Fifos/Fifo.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}

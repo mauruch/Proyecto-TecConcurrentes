81135d2f9bb79752d9b768869f2fb7be0d3f9561
mauruch <capolupo.mauro@gmail.com>
2015-10-24 21:16:04 -0300

    show more details about syscall exception

--- src/lib/syscalls/exceptions/SysCallException.h
+++ src/lib/syscalls/exceptions/SysCallException.h
@@ -8,8 +8,12 @@
 #ifndef SRC_UTILS_EXCEPTIONS_SYSCALLEXCEPTION_H_
 #define SRC_UTILS_EXCEPTIONS_SYSCALLEXCEPTION_H_
 
+#include <errno.h>
+#include <string.h>
 #include <stdexcept>
 #include <string>
+#include "../../utils/Helper.h"
+
 using std::runtime_error;
 
 class SysCallException : public runtime_error
@@ -17,7 +21,9 @@ class SysCallException : public runtime_error
 public:
 	SysCallException(): runtime_error("Error executing a system call") {}
 
-	SysCallException(const std::string& msgError): runtime_error(msgError) {}
+	SysCallException(const std::string& msgError):
+		runtime_error(msgError + ": " + Helper::convertToString(strerror(errno))) {
+	}
 };
 
 #endif /* SRC_UTILS_EXCEPTIONS_SYSCALLEXCEPTION_H_ */
